354bbe82fc54b8f0f0f975fd99733fd32f563049
SangYong-Jeong <mmaf@naver.com>
2021-11-28 17:00:43 +0900

    API 연동중

---
+++ src/components/post/PostViewr.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+import SubInfo from '../common/SubInfo';
+import Tags from '../common/Tags';
+
+const PostViewrBlock = styled(Responsive)`
+  margin-top: 4rem;
+`;
+
+const PostHead = styled.div`
+  border-bottom: 1px solid ${palette.gray[2]};
+  padding-bottom: 3rem;
+  margin-bottom: 3rem;
+  h1 {
+    font-size: 3rem;
+    line-height: 1.5;
+    margin: 0;
+  }
+`;
+
+const PostContent = styled.div`
+  font-size: 1.3125rem;
+  color: ${palette.gray[8]};
+`;
+
+const PostViewr = ({ post, error, loading }) => {
+  // 에러 발생 시
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <PostViewrBlock>존재하지 않는 포스트입니다.</PostViewrBlock>;
+    }
+    return <PostViewrBlock>오류 발생!</PostViewrBlock>;
+  }
+
+  // 로딩 중이거나 아직 포스트 데이터가 없을 때
+  if (loading || !post) {
+    return null;
+  }
+
+  const { title, body, user, publishedDate, tags } = post;
+
+  return (
+    <PostViewrBlock>
+      <PostHead>
+        <h1>{title}</h1>
+        <SubInfo
+          username={user.username}
+          publishedDate={publishedDate}
+          hasMarginTop
+        />
+        <Tags tags={tags} />
+      </PostHead>
+      <PostContent dangerouslySetInnerHTML={{ __html: body }} />
+    </PostViewrBlock>
+  );
+};
+
+export default PostViewr;
