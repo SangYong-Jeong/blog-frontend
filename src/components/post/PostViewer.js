503e43b41894b390f466d9a5807be4ad5e2c94e6
SangYong-Jeong <mmaf@naver.com>
2021-11-28 18:59:56 +0900

    블로그 프로젝트 완료

--- src/components/post/PostViewer.js
+++ src/components/post/PostViewer.js
@@ -4,11 +4,11 @@ import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
 import SubInfo from '../common/SubInfo';
 import Tags from '../common/Tags';
+import { Helmet } from 'react-helmet-async';
 
-const PostViewrBlock = styled(Responsive)`
+const PostViewerBlock = styled(Responsive)`
   margin-top: 4rem;
 `;
-
 const PostHead = styled.div`
   border-bottom: 1px solid ${palette.gray[2]};
   padding-bottom: 3rem;
@@ -25,24 +25,27 @@ const PostContent = styled.div`
   color: ${palette.gray[8]};
 `;
 
-const PostViewr = ({ post, error, loading, actionButtons }) => {
+const PostViewer = ({ post, error, loading, actionButtons, ownPost }) => {
   // 에러 발생 시
   if (error) {
     if (error.response && error.response.status === 404) {
-      return <PostViewrBlock>존재하지 않는 포스트입니다.</PostViewrBlock>;
+      return <PostViewerBlock>존재하지 않는 포스트입니다.</PostViewerBlock>;
     }
-    return <PostViewrBlock>오류 발생!</PostViewrBlock>;
+    return <PostViewerBlock>오류 발생!</PostViewerBlock>;
   }
 
-  // 로딩 중이거나 아직 포스트 데이터가 없을 때
+  // 로딩중이거나, 아직 포스트 데이터가 없을 시
   if (loading || !post) {
     return null;
   }
 
   const { title, body, user, publishedDate, tags } = post;
-
   return (
-    <PostViewrBlock>
+    <PostViewerBlock>
+      <Helmet>
+        <title>{title} - REACTERS</title>
+      </Helmet>
+
       <PostHead>
         <h1>{title}</h1>
         <SubInfo
@@ -54,8 +57,8 @@ const PostViewr = ({ post, error, loading, actionButtons }) => {
       </PostHead>
       {actionButtons}
       <PostContent dangerouslySetInnerHTML={{ __html: body }} />
-    </PostViewrBlock>
+    </PostViewerBlock>
   );
 };
 
-export default PostViewr;
+export default PostViewer;
--- src/containers/post/PostViewerContainer.js
+++ src/containers/post/PostViewerContainer.js
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { readPost, unloadPost } from '../../modules/post';
-import PostViewr from '../../components/post/PostViewr';
-import PostActionButtons from '../../components/post/PostActionButton';
+import PostViewer from '../../components/post/PostViewer';
+import PostActionButtons from '../../components/post/PostActionButtons';
 import { setOriginalPost } from '../../modules/write';
+import { removePost } from '../../lib/api/posts';
 
 const PostViewerContainer = ({ match, history }) => {
   // 처음 마운트될 때 포스트 읽기 API 요청
@@ -32,13 +33,25 @@ const PostViewerContainer = ({ match, history }) => {
     history.push('/write');
   };
 
+  const onRemove = async () => {
+    try {
+      await removePost(postId);
+      history.push('/'); // 홈으로 이동
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   const ownPost = (user && user._id) === (post && post.user._id);
+
   return (
-    <PostViewr
+    <PostViewer
       post={post}
       loading={loading}
       error={error}
-      actionButtons={ownPost && <PostActionButtons onEdit={onEdit} />}
+      actionButtons={
+        ownPost && <PostActionButtons onEdit={onEdit} onRemove={onRemove} />
+      }
     />
   );
 };
